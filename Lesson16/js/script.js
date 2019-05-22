02eaa875e840542ac7dd715091a57d5210455b1f
Oksana.Zembal <oksanazembal@gmail.com>
2019-05-22 20:53:20 +0300

    Added AFRODITA proect + create lessom 16

---
+++ Lesson16/js/script.js
@@ -0,0 +1,12 @@
+/*eslint-disable no-console */
+//eslint no - console: "error"
+// --------TASK1---------
+var skill = {
+    name: "html",
+    level: 5,
+};
+
+for (var key in skill) {
+
+    console.log(skill[key]);
+}
