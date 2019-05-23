f80d1c8fd1790a6d46dfd659be5ba60682c084bc
Oksana.Zembal <oksanazembal@gmail.com>
2019-05-23 14:33:39 +0300

    Done lesson15 tasks +lesson16

--- Lesson15/js/script.js
+++ Lesson15/js/script.js
@@ -1,39 +1,39 @@
 /*eslint-disable no-console */
 //eslint no - console: "error"
-// --------TASK1---------
-function addArreyByPush() {
-    var arr = [];
-    for (var i = 0; i == null; i++) {
-        arr[i] = arr.push(prompt());
-        if (arr[i] == null) {
-            break;
-        }
+// --------TASK1--ВВедення масиву через promt----(НЕ ЗНАЮ ЯК ЗРОБИТИ)---
+var arr = [];
+var number = 10;
+
+function addArreyByPush(number) {
+    for (var i = 0; i < number && i !== null; i++) {
+        var newarr = arr.push(+prompt("Enter the numbers:"));
+
     }
-    return arr;
+    return newarr;
 }
 console.log(addArreyByPush());
-console.log(arr);
 
-// --------TASK2---------
+// --------TASK2--Сума елементів матриці-------
+var arrmatrix = [
+    [1, 1, 1],
+    [1, 5, 1],
+    [1, 1, 1]
+];
 
-function getMatrixIndex(arr) {
+function getMatrixIndex(arrmatrix) {
     var sum = 0;
-    for (var i = 0; i < arr.length; i++) {
-        for (var j = 0; j < arr.length; j++) {
-            sum = sum + arr[i][j];
+    for (var i = 0; i < arrmatrix.length; i++) {
+        for (var j = 0; j < arrmatrix.length; j++) {
+            sum = sum + arrmatrix[i][j];
         }
     }
     return sum;
 }
-var arr = [
-    [1, 1, 1],
-    [1, 1, 1],
-    [1, 1, 1]
-];
-console.log(getMatrixIndex(arr));
 
-// --------TASK3---------
-var longString = "Lorem ipsum dolor sit amet consectetur adipisicing elit Consequuntur laboriosam nisi fuga commod adipisci in laborum quod nihil dignissimos itaque architecto recusandae corrupti";
+console.log(getMatrixIndex(arrmatrix));
+
+// --------TASK3-- Перетворення рядка в масив-------
+var longString = "Lorem ipsum dolor sit amet consectetur adipisicing elit Consequuntur laboriosam nisi fuga";
 var arrey = longString.split(' ');
 console.log(arrey);
 for (var i = 0; i < arrey.length; i++) {
@@ -54,7 +54,48 @@ function getLongStrings() {
 var result = getLongStrings();
 console.log(result);
 
-// --------TASK4------------
+// --------TASK4---Додавання елементу до масиву---------
 var numberArrey = [1, 9, 22, 7, 6];
 numberArrey.splice(3, 0, 8);
 console.log(numberArrey);
+
+// --------TASK5------------Массив випадкових чисел-------
+var myMax = 100;
+
+function randomNumber(number) {
+    var arrey = [];
+    for (var i = 0; i < number; i++) {
+        arrey.push(Math.floor(Math.random() * myMax) + 1);
+    }
+    return arrey;
+}
+var arreyResult = randomNumber(10);
+console.log(arreyResult);
+console.log(arreyResult.indexOf(+prompt("Enter number")));
+
+
+
+
+// --------TASK6-----Метод forEach(element)---- 
+var loremString = "Lorem   elit Consequuntur dolor sit fugaath";
+var words = loremString.split(' ');
+console.log(words);
+
+function getLongestWord(words) {
+    var longestWord = "";
+    words.forEach(function(word) {
+        if (word.length > longestWord.length) {
+            longestWord = word;
+        }
+    });
+    return longestWord;
+}
+console.log("The longest word is: " + getLongestWord(words));
+
+// --------TASK7-----Метод map(element)----
+var arreyOfNumbers = [8, 6, 12, 10];
+console.log(arreyOfNumbers);
+var square = arreyOfNumbers.map(function(sqr) {
+    return sqr * sqr;
+});
+console.log(square);
