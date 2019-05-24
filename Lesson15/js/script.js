3a84988a37c936e78fad73361612459bf3d1af12
Oksana.Zembal <oksanazembal@gmail.com>
2019-05-24 16:58:09 +0300

    Afrodita -bugs fixed + completed lesson 15

--- Lesson15/js/script.js
+++ Lesson15/js/script.js
@@ -1,17 +1,22 @@
 /*eslint-disable no-console */
 //eslint no - console: "error"
+
+
 // --------TASK1--ВВедення масиву через promt----(НЕ ЗНАЮ ЯК ЗРОБИТИ)---
 var arr = [];
-var number = 10;
 
-function addArreyByPush(number) {
-    for (var i = 0; i < number && i !== null; i++) {
-        var newarr = arr.push(+prompt("Enter the numbers:"));
-
+function getPushArray() {
+    var f = prompt();
+    if (f === null) {
+        console.log(arr);
+    } else {
+        arr.push(f);
+        getPushArray();
     }
-    return newarr;
 }
-console.log(addArreyByPush());
+getPushArray();
+
+
 
 // --------TASK2--Сума елементів матриці-------
 var arrmatrix = [
@@ -33,6 +38,8 @@ function getMatrixIndex(arrmatrix) {
 console.log(getMatrixIndex(arrmatrix));
 
 // --------TASK3-- Перетворення рядка в масив-------
+
+
 var longString = "Lorem ipsum dolor sit amet consectetur adipisicing elit Consequuntur laboriosam nisi fuga";
 var arrey = longString.split(' ');
 console.log(arrey);
@@ -42,6 +49,8 @@ for (var i = 0; i < arrey.length; i++) {
     }
 }
 // --------TASK3 (Function method)---------
+
+
 function getLongStrings() {
     var targetArray = [];
     for (var i = 0; i < arrey.length; i++) {
@@ -55,11 +64,13 @@ var result = getLongStrings();
 console.log(result);
 
 // --------TASK4---Додавання елементу до масиву---------
+
 var numberArrey = [1, 9, 22, 7, 6];
 numberArrey.splice(3, 0, 8);
 console.log(numberArrey);
 
 // --------TASK5------------Массив випадкових чисел-------
+
 var myMax = 100;
 
 function randomNumber(number) {
@@ -77,6 +88,7 @@ console.log(arreyResult.indexOf(+prompt("Enter number")));
 
 
 // --------TASK6-----Метод forEach(element)---- 
+
 var loremString = "Lorem   elit Consequuntur dolor sit fugaath";
 var words = loremString.split(' ');
 console.log(words);
@@ -93,6 +105,7 @@ function getLongestWord(words) {
 console.log("The longest word is: " + getLongestWord(words));
 
 // --------TASK7-----Метод map(element)----
+
 var arreyOfNumbers = [8, 6, 12, 10];
 console.log(arreyOfNumbers);
 var square = arreyOfNumbers.map(function(sqr) {
