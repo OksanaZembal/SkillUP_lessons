d475ec8dba27d2e25322f46ee50e74a5f58cfd4b
Oksana.Zembal <oksanazembal@gmail.com>
2019-06-09 17:34:03 +0300

    Bug fixing, add DOM-homework and Lesson22 classwork

---
+++ Lesson18 DOM/Task4/js/script.js
@@ -0,0 +1,19 @@
+var addLi = document.createElement('li');
+addLi.innerHTML = '1';
+list.appendChild(addLi);
+
+var addLi = document.createElement('li');
+addLi.innerHTML = '2';
+list.appendChild(addLi);
+
+var addLi = document.createElement('li');
+addLi.innerHTML = '3';
+list.appendChild(addLi);
+
+var addLi = document.createElement('li');
+addLi.innerHTML = '4';
+list.appendChild(addLi);
+
+var addLi = document.createElement('li');
+addLi.innerHTML = '5';
+list.appendChild(addLi);
---
+++ Lesson21-22 ES6/Lesson22 classwork/js/script.js
@@ -0,0 +1,18 @@
+var user = '{"name":"Vasya", "age":35, "isAdmin": false, "friends":[0,1,2,3]}';
+user = JSON.parse(user);
+alert(user.friends[1]);
+
+for (key in user) {
+    if (key != "friends") {
+        console.log(key + ":" + user[key])
+    } else if (key == "friends") {
+        var sum = 0;
+        var g = user[key];
+        for (i = 0; i < g.length; i++) {
+            sum = sum + g[i];
+            console.log("friends" + ":" + g[i]);
+        }
+        console.log("array sum elements = " + sum);
+    }
+
+};
